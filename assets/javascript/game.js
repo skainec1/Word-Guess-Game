e822eeea1499eeeb61caef09289a21908956183c
Stephen Kainec <stephen.kaienc@gmail.com>
2019-08-27 21:36:33 -0400

    adding functioning reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,33 +10,39 @@ var underScore = [];
 var winCounter = 0;
 var lossCounter = 0;
 var wrongGuessCounter = 12;
+var keycode;
+var keyWord;
+// var e = 
+
 
 // var letterBlanks = 0;
 console.log(chosenWord);
 
 // display chosen word as underscores
-function startGame() {
-    wrongGuessCounter = 12;
-    chosenWord = "";
-}
+// function startGame() {
+//     wordsIndex = Math.floor(Math.random() * words)
+//     wrongGuessCounter = 12;
+//     chosenWord = "";
+// }
 
-function generateUnderscore() {
-    for (let i = 0; i < chosenWord.length; i++) {
+function generateUnderscore(e) {
+    console.log("asd" + e);
+    for (let i = 0; i < e.length; i++) {
         underScore.push('_');
         
     }
     return underScore;
 }
-console.log(generateUnderscore());
+console.log(generateUnderscore(chosenWord));
 // document.getElementsByClassName("hiddenWord").innerHTML = generateUnderscore();
 
 // User guess
-document.addEventListener('keypress', (event) => {
-    var keycode = event.keyCode;
-    var keyWord = String.fromCharCode(keycode);
-    console.log(keyWord);
+var Userguess = function(event) {
+     keycode = event.keyCode;
+     keyWord = String.fromCharCode(keycode);
+    console.log("keyWord: " + keyWord);
     if(chosenWord.indexOf(keyWord) > -1) {
-        // console.log(true);
+        console.log(true);
         rightWord.push(keyWord);
     underScore[chosenWord.indexOf(keyWord)] = keyWord;
         console.log(underScore.join(""));
@@ -55,35 +61,45 @@ document.addEventListener('keypress', (event) => {
         winCounter++;
         alert("You Won!");
         $(".wins").text("Wins: " + winCounter);
-        roundComplete();
-        startGame();
+        // roundComplete();
+        
         console.log("wins: " + winCounter);
         
     };
     if (wrongWord.length > 11) {
         lossCounter++;
         alert("You Lose!");
-        roundComplete();
-        startGame();
+        // roundComplete();
+       
         console.log("Losses: " + lossCounter);
     };
-    newWord.addEventListener("click" ,restartGame);
-
-    function restartGame() {
-    generateUnderscore();
-    wrongGuessCounter = 12;
-    
-    }
-    
-   function roundComplete() {
-       document.getElementsByClassName("remainingGuesses").innerHTML = wrongGuessCounter;
-       document.getElementsByClassName("hiddenWord").innerHTML = underScore;
-       document.getElementsByClassName("wins").innerHTML = winCounter;
-   }
-    
-  
-
+   
+};
+document.addEventListener('keypress', function (event) {
+    Userguess(event);
 });
+console.log(chosenWord);
+
+
+$("#newWord").click(function restartGame(){
+    wordsIndex = Math.floor(Math.random() * words.length);
+    chosenWord = words[wordsIndex];
+    
+    wrongGuessCounter = 12;
+    $(".remainingGuesses").text("Wrong Guesses Remaining: " + wrongGuessCounter);
+    rightWord = [];
+    wrongWord = [];
+    $(".wrongGuesses").text(wrongWord);
+    underScore = [];
+    $(".hiddenWord").text(underScore.join(""));
+    console.log(chosenWord);
+    generateUnderscore(chosenWord);
+    Userguess();
+    // $(".hiddenWord").text(underScore[chosenWord.indexOf(keyWord)]);
+    console.log("clicks");
+    console.log(chosenWord);
+    console.log(wordsIndex);
+    });
 
 
 
